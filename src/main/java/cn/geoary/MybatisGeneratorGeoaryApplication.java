87c1ff531bfbe1e1c5ba8d5dabce15a20ec7be38
Zhangchao999 <17803477121@163.com>
2019-08-18 21:52:30 +0800

    mybatis-gengerator
    mybatis 逆向工程demo

---
+++ src/main/java/cn/geoary/MybatisGeneratorGeoaryApplication.java
@@ -0,0 +1,13 @@
+package cn.geoary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGeneratorGeoaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisGeneratorGeoaryApplication.class, args);
+    }
+
+}
